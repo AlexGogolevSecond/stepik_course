4e9781681986713a862b4c3fa73d0d6064d1908b
Aleksandr <a.gogolev@carbonsoft.ru>
2024-07-14 23:30:04 +0500

    loops

--- chapter1/loops1.go
+++ chapter1/loops1.go
@@ -9,5 +9,22 @@ func main(){
 		fmt.Println(i)
 		sum += i
 	}
-	fmt.Println(sum)	
+	fmt.Println(sum)
+	fmt.Println("=============")
+
+	var j = 1
+	for ; j < 10; j++{
+		//fmt.Println(j)
+	    fmt.Println(j * j)
+	}
+
+	fmt.Println("==============")
+	//аналог цикла while !!!
+	var k = 1
+	for k < 10 {
+		fmt.Println(k * k)
+		k++
+	}
+
+
 }
