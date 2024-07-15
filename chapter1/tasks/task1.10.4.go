eb7448268c5d76a6029803de446396a20e6d3f70
Aleksandr <a.gogolev@carbonsoft.ru>
2024-07-15 23:48:30 +0500

    tasks

--- chapter1/tasks/task1.10.4.go
+++ chapter1/tasks/task1.10.4.go
@@ -32,16 +32,18 @@ func main(){
 		max_elem int
 		count_max_elem int
 	)
-
+	a = -1
 	max_elem = 0
 	count_max_elem = 0
 	for a != 0 {
 		fmt.Scan(&a)
 		if a > max_elem {
+			count_max_elem = 0
 			max_elem = a
 			count_max_elem += 1
 		} else if a == max_elem {
 			count_max_elem += 1
 		}
 	}
+	fmt.Println(count_max_elem)
 }
